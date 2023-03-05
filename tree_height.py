32756c21fea63ee2df5fa763f16a4011683a065e
221RDB167 <124889522+221RDB167@users.noreply.github.com>
2023-03-05 20:41:33 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,52 @@
-# python3
-
 import sys
-import threading
-import numpy
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+def build_tree(n, parents):
+    nodes = [Node(i) for i in range(n)]
+
+    for i in range(n):
+        parent = parents[i]
+
+        if parent == -1:
+            root = nodes[i]
+        else:
+            nodes[parent].children.append(nodes[i])
+
+    return root
+
+def get_height(root):
+    if not root.children:
+        return 1
+
+    max_depth = 0
+
+    for child in root.children:
+        max_depth = max(max_depth, get_height(child))
+
+    return max_depth + 1
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
-
+    root = build_tree(n, parents)
+    return get_height(root)
 
 def main():
-    # implement input form keyboard and from files
     
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    h = int(input())
+    if h == "I":
+        n = int(input())   
+        parents = list(map(int, input().split()))
+        
+    else :
+        h = int(input())
+        
+    print(compute_height(n, parents))
 
 # In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+# so raise it here for this problem.
+sys.setrecursionlimit(10**7)
+
 main()
-# print(numpy.array([1,2,3]))
